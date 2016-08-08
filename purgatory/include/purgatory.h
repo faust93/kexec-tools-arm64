ac0ab87b994f4aaf9e1ffb212216f5afd5de0085
Geoff Levand <geoff@infradead.org>
2016-08-08 14:52:54 -0700

    debugging: Add model_brk

--- purgatory/include/purgatory.h
+++ purgatory/include/purgatory.h
@@ -8,4 +8,16 @@ void printf(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 void setup_arch(void);
 void post_verification_setup_arch(void);
 
+static inline void model_brk(int line)
+{
+	asm volatile(
+		"mov x0, #0x18;"	/* angel_SWIreason_ReportException */
+		"mov x1, #0x20000;"
+		"add x1, x1, #0x20;"	/* ADP_Stopped_BreakPoint */
+		"hlt #0xf000\n"		/* A64 semihosting */
+		:
+		: "r" (line)
+		: "x0", "x1");
+}
+
 #endif /* PURGATORY_H */
